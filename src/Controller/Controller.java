5aed5a775f0eab34462423e444ad4151544e52c7
FrScarlato <fr.scarlato@studenti.unina.it>
2025-05-25 12:44:59 +0200

    Add files via upload

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,27 +1,5 @@
-package Controller;
-import ClassModel.*;
-import java.util.LinkedList;
-
 public class Controller {
 
-    private Utente UtenteCorrente;
-    private static LinkedList<Utente> UtentiRegistrati = new LinkedList<>();
 
-    public boolean TrovaUtente(String NomeUtente, String PasswordUtente)
-    {
-        if(NomeUtente.length() < 5  || PasswordUtente.length() < 8) {
-                return false;
-        }
-        else {
-            Utente UtenteTmp = new Utente(NomeUtente, PasswordUtente);
-            for(Utente u : UtentiRegistrati) {
-                if(UtenteTmp.equals(u)) {
-                    return false;
-                }
-            }
-            UtentiRegistrati.addLast(UtenteTmp);
-            UtenteCorrente = UtenteTmp;
-            return true;
-        }
-    }
+
 }
